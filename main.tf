a99ddc966b90112d4941b4af0b382314e4ebd6d7
Christian Rebischke <chris@shibumi.dev>
2020-02-18 15:24:24 +0100

    add latest terraform server import

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "hcloud_server" "kurisu" {
+  name        = "kurisu"
+  server_type = "cx11-ceph"
+  location    = "fsn1"
+  image       = "fedora-31"
+  lifecycle {
+    ignore_changes = [image]
+  }
+}
+
+resource "hcloud_rdns" "kurisu" {
+  server_id  = hcloud_server.kurisu.id
+  ip_address = hcloud_server.kurisu.ipv4_address
+  dns_ptr    = "kurisu.shibumi.dev"
+}
